31c4e70a79f5ff4a8684bc5ab189103ac7c9e06d
Nikolay Yuchormanski <yuchormanski@gmail.com>
2023-12-02 11:15:04 +0200

    added Geo Location hook

---
+++ Hooks/useGeoLocation.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+// import this hook in component where you need position
+// use getPosition
+
+function useGeolocation(defaultPosition = null) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [position, setPosition] = useState(defaultPosition);
+  const [error, setError] = useState(null);
+
+  function getPosition() {
+    if (!navigator.geolocation)
+      return setError("Your browser does not support geolocation");
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+    console.log(position);
+  }
+
+  return { isLoading, position, error, getPosition };
+}
+
+export { useGeolocation };
